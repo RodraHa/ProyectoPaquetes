3fb16cad9b9a46a8a88911a00a5f4f173e1ff834
Josue Peralta <peraltajosue115@gmail.com>
2024-08-04 21:05:12 -0500

    cambios
    comentarios para clases y metodos

--- src/mod_incidentes/PaquetePerdido.java
+++ src/mod_incidentes/PaquetePerdido.java
@@ -1,20 +1,36 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change esta licencia
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java para editar esta plantilla
  */
 package mod_incidentes;
 
 /**
+ * Clase que representa un incidente de paquete perdido.
+ * Esta clase extiende la clase abstracta Incidente y proporciona
+ * implementaciones específicas para registrar y resolver el incidente.
  *
- * @author Rodrigo Haro
+ * @autor Rodrigo Haro
  */
 public class PaquetePerdido extends Incidente {
 
+    /**
+     * Registra el incidente de paquete perdido.
+     *
+     * @return Una cadena que describe el incidente registrado.
+     */
     @Override
     public String registrar() {
         return "La ubicación de su paquete es desconocida";
     }
 
+    /**
+     * Resuelve el incidente de paquete perdido.
+     *
+     * @param argumentos Argumentos necesarios para resolver el incidente.
+     *                   El primer argumento debe ser una cadena que indica si la resolución
+     *                   es "Recuperado" o "Desconocido".
+     * @return Una cadena que describe la resolución del incidente.
+     */
     @Override
     public String resolver(String[] argumentos) {
         if (argumentos[0].equals("Recuperado")) {
